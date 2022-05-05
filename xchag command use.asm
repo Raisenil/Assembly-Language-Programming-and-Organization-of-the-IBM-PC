61cd5b5431e209e092bbe3fc78af591ea7e02e1f
Raisenil <raisulislamniloy47@gmail.com>
2022-05-06 00:23:39 +0600

    exercise

---
+++ xchag command use.asm
@@ -0,0 +1,47 @@
+;Display '?'.Read 2 capitals and display in alphabetical order
+.model small
+.stack 100h
+.code
+main proc
+    mov ah,2
+    mov dl,'?'
+    int 21h
+    
+    mov ah,1
+    int 21h
+    mov bl,al 
+    
+    mov ah,1
+    int 21h
+    mov bh,al  
+      
+    cmp bh,bl
+    jg switch
+    jmp display
+    
+    switch:
+    xchg bl,bh
+    jmp display
+    
+    display:
+    mov ah,2
+    mov dl,0ah
+    int 21h
+    mov dl,0dh
+    int 21h
+    
+    mov ah,2
+    mov dl,bh
+    int 21h
+    
+    mov ah,2
+    mov dl,bl
+    int 21h
+    
+    exit:
+    mov ah,4ch
+    int 21h
+    
+    main endp
+end main
+    
